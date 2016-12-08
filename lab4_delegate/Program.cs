bf2b057c6d5c3eb40fcc0db17fbc80f343750258
Katerina Gordiyenko <KaterinaO.Gordiyenko@livenau.net>
2016-12-08 10:31:09 +0200

    delete comment

--- lab4_delegate/Program.cs
+++ lab4_delegate/Program.cs
@@ -14,35 +14,7 @@ namespace lab4_delegate
                 u.menu();
                 u.casein();
             }
-            //    try
-            //    {
-            //        СStack<string> stack = new СStack<string>(8);
-            //        // добавляем четыре элемента
-            //        stack.Push("Kate");
-            //        stack.Push("Sam");
-            //        stack.Push("Alice");
-            //        stack.Push("Tom");
-            //        stack.Push("A");
-            //        stack.Push("B");
-            //        stack.Push("C");
-            //        stack.Push("D");
-            //        //stack.Push("E");
-            //        // извлекаем один элемент
-            //        var head = stack.Pop();
-            //        Console.WriteLine(head);    // Tom
-
-            //        // просто получаем верхушку стека без извлечения
-            //        head = stack.Peek();
-            //        Console.WriteLine(head);    // Alice
-            //    }
-            //    catch (InvalidOperationException ex)
-            //    {
-            //        Console.WriteLine(ex.Message);
-            //    }
-
-
-            //    Console.Read();
-            //}
+            
         }
         
     }
